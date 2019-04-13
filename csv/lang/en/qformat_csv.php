5277c97b60a0f96b4c82f23fed19be36e89193cc
hunterhqm <hunterhqm@users.noreply.github.com>
2019-04-13 19:58:54 +0800

    multichoice,turefalse is supported

--- csv/lang/en/qformat_csv.php
+++ csv/lang/en/qformat_csv.php
@@ -23,7 +23,11 @@
  */
 
 $string['pluginname'] = 'CSV format';
-$string['pluginname_help'] = 'This is a CSV format for importing and exporting multiple choice questions (All-or-nothing plugin must be installed) more then one answers from a CSV(Comma separated value) file. Please find the Sample csv <a href="/question/format/csv/sample.csv">sample.csv</a> file for your reference.';
+$string['pluginname_help'] = 'This is a CSV format for importing multiple choice questions (All-or-nothing plugin must be installed)£¬truefalse questions from a CSV(Comma separated value) file. Please find the Sample csv file for your reference.<br />
+		<a href="/question/format/csv/multichoice.csv">multichoice.csv</a><br />
+		<a href="/question/format/csv/sampleTruefalse.csv">sampleTruefalse.csv</a><br />
+		<a href="/question/format/csv/sampleShortanswer.csv">sampleShortanswer.csv</a><br />
+		<a href="/question/format/csv/sampleEssay.csv">sampleEssay.csv</a><br />';
 $string['pluginname_link'] = 'qformat/csv';
 $string['commma_error'] = '<font color="#990000"> Upload failed. Unnecessary Comma(,) found in <b> Question {$a} </b><br /> Please remove the comma(,) from the field or Put the entire text between the double quotes(" "), So the coomma(,) between them can be ignored. <br /></font>';
 $string['newline_error'] = '<font color="#990000">Upload failed. New Line found in <b> Question {$a} . </b> Make sure that entire question with choices and answers are in one line itself.<br /> Please correct this question and try importing again. <br /> No Question has been imported.</font>';
--- csv/lang/zh_cn/qformat_csv.php
+++ csv/lang/zh_cn/qformat_csv.php
@@ -8,26 +8,29 @@
 //
 // Moodle is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+// along with Moodle. If not, see <http://www.gnu.org/licenses/>.
 
 /**
  * Strings for component 'qformat_csv', language 'en'
  *
- * @package    qformat_csv
- * @copyright  2018 Gopal Sharma <gopalsharma66@gmail.com>
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package qformat_csv
+ * @copyright 2018 Gopal Sharma <gopalsharma66@gmail.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
-$string['pluginname'] = 'CSV 格式';
-$string['pluginname_help'] = '此插件是导入CSV格式的多选题（All-or-nothing 插件必须安装），各选项以英文“;”分隔，格式请参考示例文件<a href="/question/format/csv/sample.csv">sample.csv</a>。';
-$string['pluginname_link'] = 'qformat/csv';
-$string['commma_error'] = '<font color="#990000"> 上传失败，在试题 <b> Question {$a} </b>发现多余的英文“,”。<br /> 请移除该“”，或者将整段文字以" "括起来。 <br /></font>';
-$string['newline_error'] = '<font color="#990000">上传失败，在试题<b> Question {$a} </b>中发现新行， 请将整个试题和答案写在一行中。<br /> </font>';
-$string['csv_file_error'] = '<font color="#990000">上传失败。在第 <b/> {$a} . </b>行中发现列数与标题行不匹配。<br /> 未导入试题。</font>';
-$string['csv_choices_error'] = '<font color="#990000">上传失败。请确认试题 <b> Question {$a} . </b>的选项以英文“;”分隔，且至少有3个选项。<br />未导入试题。</font>';
-$string['noquestion_error'] = '<font color="#990000">上传失败，未发现1个试题。<br />未导入试题。</font>';
-$string['noanswer_error'] = '<font color="#990000">上传失败，请确保 <b> Question {$a} </b>至少有一个正确答案。<br /> 未导入试题。</font>';
+$string ['pluginname'] = 'CSV 格式';
+$string ['pluginname_help'] = '此插件是导入CSV格式的多选题（All-or-nothing 插件必须安装，各选项以英文“;”分隔），判断题格式请参考示例文件：<br/>
+		<a href="/question/format/csv/multichoice.csv">multichoice.csv</a><br />
+		<a href="/question/format/csv/sampleTruefalse.csv">sampleTruefalse.csv</a><br />
+		<a href="/question/format/csv/sampleShortanswer.csv">sampleShortanswer.csv</a><br />
+		<a href="/question/format/csv/sampleEssay.csv">sampleEssay.csv</a><br />';
+$string ['pluginname_link'] = 'qformat/csv';
+$string ['commma_error'] = '<font color="#990000"> 上传失败，在试题 <b> Question {$a} </b>发现多余的英文“,”。<br /> 请移除该“”，或者将整段文字以" "括起来。 <br /></font>';
+$string ['newline_error'] = '<font color="#990000">上传失败，在试题<b> Question {$a} </b>中发现新行， 请将整个试题和答案写在一行中。<br /> </font>';
+$string ['csv_file_error'] = '<font color="#990000">上传失败。在第 <b/> {$a} . </b>行中发现列数与标题行不匹配。<br /> 未导入试题。</font>';
+$string ['csv_choices_error'] = '<font color="#990000">上传失败。请确认试题 <b> Question {$a} . </b>的选项以英文“;”分隔，且至少有3个选项。<br />未导入试题。</font>';
+$string ['noquestion_error'] = '<font color="#990000">上传失败，未发现1个试题。<br />未导入试题。</font>';
+$string ['noanswer_error'] = '<font color="#990000">上传失败，请确保 <b> Question {$a} </b>至少有一个正确答案。<br /> 未导入试题。</font>';
